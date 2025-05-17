8f86a88bbe157cfa40cfdf47088bbc07d09e0068
SergioAle210 <ore221122@uvg.edu.gt>
2025-05-17 11:38:47 -0600

    FEAT: Se implementó que se pueda medir las métricas

--- scheduler/utils.py
+++ scheduler/utils.py
@@ -11,3 +11,19 @@ def leer_procesos(path):
                     Proceso(pid.strip(), bt.strip(), at.strip(), pr.strip())
                 )
     return procesos
+
+
+def calcular_metricas(procesos):
+    total_wt = 0
+    total_tat = 0
+    n = len(procesos)
+
+    for p in procesos:
+        wt = p.start_time - p.at
+        tat = p.end_time - p.at
+        total_wt += wt
+        total_tat += tat
+
+    avg_wt = total_wt / n
+    avg_tat = total_tat / n
+    return avg_wt, avg_tat
